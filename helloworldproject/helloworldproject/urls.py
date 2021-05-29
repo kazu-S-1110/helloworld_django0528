c8170da8c6b6c8909c6b4429e903bec7e4c6ed7b
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-05-29 23:30:12 +0900

    viewを書いてみた

--- helloworldproject/helloworldproject/urls.py
+++ helloworldproject/helloworldproject/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from .view import helloworldfunction
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("hello/", helloworldfunction)
 ]
