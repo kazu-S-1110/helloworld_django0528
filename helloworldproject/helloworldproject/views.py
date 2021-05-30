4027b365e8535af46307ee8ae4a29db011371ce7
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-05-30 21:56:38 +0900

    TemplatesフォルダとBase_Dirを理解

--- helloworldproject/helloworldproject/views.py
+++ helloworldproject/helloworldproject/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
+from django.views.generics import TemplateView
 
 
 def helloworldfunction(request):
     returnobject = HttpResponse("<h1>hello world</h1>")
     return returnobject
+
+
+class HelloWorldView(TemplateView):
+    template_name = "hello.html"  # HelloWorldViewが呼び出されたらhello.htmlを返すよう記述している
